5c7d32fe532f3a0b06b46c74d349329a13cf14a9
Lukman Hakim <hlukman156@gmail.com>
2022-06-09 21:30:55 +0700

    change image to link image

--- cafe/models.py
+++ cafe/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Reviewer(models.Model):
     account = models.ForeignKey(User, on_delete=models.CASCADE)
     bio = models.CharField("Bio", max_length=120, help_text="Ceritakan diri kamu dalam 120 character!")
-    picture = models.ImageField("Profile Picture", null=True)
+    picture = models.CharField("Link Gambar", max_length=120, null=True)
 
     def __str__(self):
         return self.account.username
@@ -20,7 +20,7 @@ class Cafes(models.Model):
     lat = models.CharField("Latitude", max_length=50)
     gmaps = models.CharField("Google Maps Link", max_length=120, help_text="Isi dengan link google maps cafe")
     is_open_24h = models.BooleanField("Buka 24 Jam", default=False)
-    image = models.ImageField("Cafe image", null=True)
+    image = models.CharField("Link Gambar", max_length=120, null=True)
 
     created_at = models.DateField("Created At", auto_now_add=True)
     updated_at = models.DateField("Updated At", default=timezone.now)
@@ -38,7 +38,7 @@ class CafeReview(models.Model):
 
 class CafePromo(models.Model):
     cafe = models.ForeignKey(Cafes, on_delete=models.CASCADE)
-    image = models.ImageField("Gambar")
+    image = models.CharField("Link Gambar", max_length=120, null=True)
     promo_title = models.CharField("Judul (Title) Promo", max_length=100)
     promo_text = models.TextField()
 
