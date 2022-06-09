52f4638b7e79274946fc8843b2f372c6e42c24a8
Lukman Hakim <hlukman156@gmail.com>
2022-06-09 21:10:59 +0700

    update setting

--- nongkies/settings.py
+++ nongkies/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://nongkies.herokuapp.com", "http://nongkies.herokuapp.com"]
 
 
 # Application definition
